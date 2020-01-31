00b2873b1977f658b81150c8e4d1dda304cb408b
keerthiv1119 <44571328+keerthiv1119@users.noreply.github.com>
2020-01-31 11:17:30 +0530

    Add files via upload

---
+++ Roman_Int.py
@@ -0,0 +1,11 @@
+def roman_int(s):
+    dictionary = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+    s = s.replace("IV","IIII").replace("IX","VIIII")
+    s = s.replace("XL","XXXX").replace("XC","LXXXX")
+    s = s.replace("CD","CCCC").replace("CM","DCCCC")
+    numeral = 0
+    for i in s:
+        numeral += dictionary[i]
+    return numeral
+Roman = input()
+print(roman_int(Roman))
